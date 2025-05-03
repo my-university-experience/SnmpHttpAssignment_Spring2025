ece103d974df630525a87f1333fe2b72a15e8aee
Yazan Al-Sedih <yazansedih@gmail.com>
2025-05-03 17:24:47 +0300

    Delete JDKTest file

--- JDKTest.java
+++
@@ -1,6 +0,0 @@
-public class JDKTest {
-    public static void main(String[] args) {
-        System.out.println("JDK is working!");
-        System.out.println("Java version: " + System.getProperty("java.version"));
-    }
-}
